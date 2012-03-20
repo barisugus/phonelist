af66be90a1f9a8f22a8424d9f2489c8c99c1a671
Baris <barisugus@gmail.com>
2012-03-20 18:16:10 +0200

    ilk commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phonelist::Application.config.secret_token = '03738d4999669253b54ec235bab598e8c275e6d67d7f0f9918df899c6390438b239471e84dc58b324cd0720b41d36747c2b7bae71b33fc23306a2dc595626d1b'
